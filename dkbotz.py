117fa9b5d9f69e7316887470721367c5d556b8d9
Don-Hai-Hum <116131095+Don-Hai-Hum@users.noreply.github.com>
2022-11-06 14:22:08 +0530

    Added Basic Things

--- dkbotz.py
+++ dkbotz.py
@@ -1,1 +1,21 @@
+from pyrogram import Client
 
+api_id = 7651392
+api_hash = "db62aa57ef8162bb4c95d0cf81e1c09b"
+bot_token = "5707765751:AAHNPzjXHoT-cdVmsJqqUyXUsRTRHbFRlkI"
+
+dkbotz = Client(
+    "ID-FINDER-BOT",
+    api_id=api_id, api_hash=api_hash,
+    bot_token=bot_token
+)
+
+
+
+
+
+
+
+
+
+dkbotz.run()
