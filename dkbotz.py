32a53142ab6b2962659b94df7ff4b7a4f0e9c250
Dogssttt <116238496+Dogssttt@users.noreply.github.com>
2022-11-12 15:14:50 +0530

    Update dkbotz.py

--- dkbotz.py
+++ dkbotz.py
@@ -10,15 +10,22 @@ dkbotz = Client(
     bot_token=bot_token
 )
 
-ft = "Hi {message.from_user.mention}\n\nDue To Overload "
+ft = "Hi {message.from_user.mention}\n\nDue To Overload Only Channel Subscribers Can Use Me\n\n Join : @DK_BOTZ"
+channel = "DK_BOTZ"
+
 
 @dkbotz.on_message(filters.private & filters.command("start"))
 async def start_cmd(client, message):
-    Fsub = await force_sub(client, message, ft
+    Fsub = await force_sub(client, message, ft, channel)
+    if Fsub == True:
+       return 
     await message.reply(f"Hi {message.from_user.first_name}, \n\nI AM ID FINDER BOT\n\nSend /help For Info")
 
 @dkbotz.on_message(filters.private & filters.command("help"))
 async def help_cmd(client, message):
+    Fsub = await force_sub(client, message, ft, channel)
+    if Fsub == True:
+       return
     await message.reply(f"How To Use Me\n\n/id - To Get Your\n\n/info - Get Your Profile Information")
 
 
