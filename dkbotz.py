2b45346cb3fca97ee5a819b1a4a7c44fb754272f
Suraj Singh <116131095+Don-Hai-Hum@users.noreply.github.com>
2022-11-26 16:25:53 +0530

    Update dkbotz.py

--- dkbotz.py
+++ dkbotz.py
@@ -35,6 +35,24 @@ async def id_cmd(client, message):
        return
     await message.reply(f"Your ID : {message.from_user.id}")
 
+@dkbotz.on_message(filters.private & filters.command("dc"))
+async def dc_cmd(client, message):
+    Fsub = await force_sub(client, message, ft, channel)
+    if Fsub == True:
+       return
+    await message.reply(f"DC ID : {message.from_user.dc_id}")
+
+
+@dkbotz.on_message(filters.private & filters.command("info"))
+async def info_cmd(client, message):
+    Fsub = await force_sub(client, message, ft, channel)
+    if Fsub == True:
+       return
+    id += "Your Details\n\n"
+    id += "First Name: {message.from_user.first_name\n\n"
+    id += "Last Name: {message.from_user.last_name\n\n"
+    await message.reply(id, quote= True)
+
 
 print("Bot Started")
 dkbotz.run()
